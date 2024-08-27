2fa1501b6c97684d42697f21edaed1a9e19853c1
Tim Lisemer <timlisemer@gmail.com>
2024-08-27 19:37:02 +0200

    move hardware config

--- common/common.nix
+++ common/common.nix
@@ -7,7 +7,6 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../hardware-configuration.nix
       ./home-manager.nix
       ../packages/packages.nix
       ../desktop-environments/desktop-enviroments.nix
--- hosts/tim-laptop.nix
+++ hosts/tim-laptop.nix
@@ -5,6 +5,7 @@
   # Import the common configuration shared across all machines
   imports = [
     ../common/common.nix
+    ./tim-laptop-hardware-configuration.nix
   ];
 
   # Machine specific configurations
