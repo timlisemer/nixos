c80818a48ce42ff65ca6ec354cc11ff8ecdf9a61
timlisemer <timlisemer@gmail.com>
2024-11-19 16:13:02 +0100

    qemu spice & remote viewer

--- common/qemu.nix
+++ common/qemu.nix
@@ -11,30 +11,30 @@
     '')
   ];
 
-  # Create a .desktop entry for the Windows 11 VM
+  # Create a .desktop entry for the Windows 11 VM -- https://www.spice-space.org/download.html
   home.file.".local/share/applications/windows11-qemu.desktop".text = ''
     [Desktop Entry]
     Name=Windows 11
     Comment=Launch Windows 11 in QEMU
-    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-11"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 11; quickemu --vm "$VM_FILES/windows-11.conf"'
+    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-11"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 11; quickemu --vm "$VM_FILES/windows-11.conf" --display none; sleep 2; remote-viewer spice://localhost:5930'
     Icon=/home/tim/.local/share/icons/windows11-48.png
     Terminal=false
     Type=Application
     Categories=System;Virtualization;
-    StartupWMClass=.qemu-system-x86_64-wrapped
+    StartupWMClass=remote-viewer
   '';
 
-  # Create a .desktop entry for the Windows 10 VM
+  # Create a .desktop entry for the Windows 10 VM -- https://www.spice-space.org/download.html
   home.file.".local/share/applications/windows10-qemu.desktop".text = ''
     [Desktop Entry]
     Name=Windows 10
     Comment=Launch Windows 10 in QEMU
-    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-10"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 10; quickemu --vm "$VM_FILES/windows-10.conf"'
+    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-10"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 10; quickemu --vm "$VM_FILES/windows-10.conf" --display none; sleep 2; remote-viewer spice://localhost:5930'
     Icon=/home/tim/.local/share/icons/windows10-48.png
     Terminal=false
     Type=Application
     Categories=System;Virtualization;
-    StartupWMClass=.qemu-system-x86_64-wrapped
+    StartupWMClass=remote-viewer
   '';
 
   # Ensure the Windows 11 icon is placed in the correct location
