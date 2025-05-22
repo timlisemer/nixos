035626487743e546f2ad91a3c8f5e0a97aa49913
timlisemer <timlisemer@gmail.com>
2025-05-22 22:23:07 +0200

    Update development.nix to remove unstable prefix from Rust and Cargo packages

--- packages/development.nix
+++ packages/development.nix
@@ -29,7 +29,7 @@ in {
         dbaeumer.vscode-eslint
         bbenoist.nix
         tauri-apps.tauri-vscode
-        unstable.vscode-extensions.rust-lang.rust-analyzer
+        rust-lang.rust-analyzer
         njpwerner.autodocstring
         svelte.svelte-vscode
         tamasfe.even-better-toml
@@ -115,9 +115,9 @@ in {
     eslint_d
     pkg-config
     gobject-introspection
-    unstable.cargo
-    unstable.cargo-edit
-    unstable.cargo-tauri
+    cargo
+    cargo-edit
+    cargo-tauri
     nodejs
     at-spi2-atk
     atkmm
