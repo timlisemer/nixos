5ab80661c5d04880af20546a00bf32207bf21cc1
timlisemer <timlisemer@gmail.com>
2025-08-03 05:05:25 +0200

    Refactor QEMU desktop entries: update Windows 11 configuration and remove Windows 10 entry

--- common/qemu.nix
+++ common/qemu.nix
@@ -18,21 +18,12 @@
     [Desktop Entry]
     Name=Windows 11
     Comment=Launch Windows 11 in QEMU
-    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-11"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 11; quickemu --vm "$VM_FILES/windows-11.conf" --display spice --viewer remote-viewer --width 800 --height 800'
-    Icon=.local/share/icons/windows11-48.png
-    Terminal=false
-    Type=Application
-    Categories=System;Virtualization;
-    StartupWMClass=remote-viewer
-  '';
 
-  # Create a .desktop entry for the Windows 10 VM -- https://www.spice-space.org/download.html
-  home.file.".local/share/applications/windows10-qemu.desktop".text = ''
-    [Desktop Entry]
-    Name=Windows 10
-    Comment=Launch Windows 10 in QEMU
-    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-10"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 10; quickemu --vm "$VM_FILES/windows-10.conf" --display spice --viewer remote-viewer --width 800 --height 800'
-    Icon=.local/share/icons/windows10-48.png
+    Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-11"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 11; quickemu --vm "$VM_FILES/windows-11.conf" --display spice --viewer remote-viewer --width 800 --height 800 --extra_args "-drive media=cdrom,index=3,file=windows-11/virtio-win.iso"'
+    # If this faulils try without --display spice
+    # Exec=bash -c 'VM_FILES="$HOME/.config/qemu/windows-11"; [ ! -d "$VM_FILES" ] || [ -z "$(ls -A "$VM_FILES")" ] && mkdir -p "$VM_FILES" && cd "$VM_FILES" && quickget windows 11; quickemu --vm "$VM_FILES/windows-11.conf" --viewer remote-viewer --width 800 --height 800 --extra_args "-drive media=cdrom,index=3,file=windows-11/virtio-win.iso"'
+
+    Icon=.local/share/icons/windows11-48.png
     Terminal=false
     Type=Application
     Categories=System;Virtualization;
@@ -41,5 +32,4 @@
 
   # Ensure the Windows 11 icon is placed in the correct location
   home.file.".local/share/icons/windows11-48.png".source = builtins.toPath ../files/icons/windows11-48.png;
-  home.file.".local/share/icons/windows10-48.png".source = builtins.toPath ../files/icons/windows10-48.png;
 }
