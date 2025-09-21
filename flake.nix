649c2b26107f66a1dfe700daf109cffc44b66dc5
timlisemer <timlisemer@gmail.com>
2025-09-21 20:17:06 +0200

    add userGameFiles backup paths

--- flake.nix
+++ flake.nix
@@ -131,12 +131,14 @@
 
     userBackupDirs = ["Coding" "Desktop" "Documents" "Pictures" "Videos" "Music" "Public" "Templates"];
     userDotFiles = [".config" ".mozilla" ".bash_history" ".steam" ".vscode-server" ".arduinoIDE" ".npm" ".vscode"];
+    userGameFiles = [".local/share/Steam/steamapps/compatdata/916440/pfx/drive_c/users/steamuser/Documents/Anno 1800/accounts/6f5db650-dcf0-4fca-9b67-21a7c8ac7dc1" ".local/share/Paradox Interactive/" ".local/share/Steam/steamapps/compatdata/916440/pfx/drive_c/users/steamuser/Documents/Anno 1800/mods"];
     backupPaths = builtins.concatLists (builtins.map (
       username: let
         h = "/home/${username}/";
       in
         (map (dir: "${h}${dir}") userBackupDirs)
         ++ (map (dir: "${h}${dir}") userDotFiles)
+        ++ (map (dir: "${h}${dir}") userGameFiles)
     ) (builtins.attrNames users));
   in {
     mkSystem = {
