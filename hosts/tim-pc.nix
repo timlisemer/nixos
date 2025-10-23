0aef4e05893684edbbe8395b94f7e9421c3642e5
timlisemer <timlisemer@gmail.com>
2025-10-23 22:48:07 +0200

    refactor(tim-pc): comment out OpenRGB service configuration
    - Commented out the OpenRGB service configuration and related environment packages in the tim-pc.nix file for potential future use.
    - This change simplifies the current configuration by removing unnecessary active settings while retaining them for reference.

--- hosts/tim-pc.nix
+++ hosts/tim-pc.nix
@@ -56,22 +56,21 @@
 
   # services.udev.extraRules = builtins.readFile ../files/OpenRGB/60-openrgb.rules;
 
-  environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
-
-  services.hardware.openrgb.enable = true;
-  systemd.user.services.openrgb = {
-    description = "OpenRGB";
-    wantedBy = ["default.target" "graphical-session.target"];
-    partOf = ["graphical-session.target"];
-    serviceConfig = {
-      # ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized --profile 'On'";
-      # ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
-      ExecStart = "/bin/openrgb --startminimized --profile 'On'";
-      Restart = "on-failure";
-      RestartSec = "5s";
-      StartLimitIntervalSec = "10s";
-      StartLimitBurst = "10";
-      After = ["graphical-session.target" "network-online.target"];
-    };
-  };
+  # environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
+  # services.hardware.openrgb.enable = true;
+  # systemd.user.services.openrgb = {
+  #   description = "OpenRGB";
+  #   wantedBy = ["default.target" "graphical-session.target"];
+  #   partOf = ["graphical-session.target"];
+  #   serviceConfig = {
+  #     # ExecStart = "${pkgs.openrgb-with-all-plugins}/bin/openrgb --startminimized --profile 'On'";
+  #     # ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
+  #     ExecStart = "/bin/openrgb --startminimized --profile 'On'";
+  #     Restart = "on-failure";
+  #     RestartSec = "5s";
+  #     StartLimitIntervalSec = "10s";
+  #     StartLimitBurst = "10";
+  #     After = ["graphical-session.target" "network-online.target"];
+  #   };
+  # };
 }
