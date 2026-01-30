53571af5f28853c724e268ccb39976012e0c4efc
timlisemer <timlisemer@gmail.com>
2026-01-31 00:12:21 +0100

    add umask=0077 mount option to /boot filesystem

--- common/disko.nix
+++ common/disko.nix
@@ -49,6 +49,7 @@ in {
                   type = "filesystem";
                   format = "vfat";
                   mountpoint = "/boot";
+                  mountOptions = ["umask=0077"];
                 };
               };
               root = {
